8b342d70d584e3fdb20b12640f8730566d285665
Andres Jared Vela Canizales <andresvela_202@hotmail.com>
2020-07-26 02:34:30 -0500

    programa que suma

---
+++ prog2.py
@@ -0,0 +1,6 @@
+for numVeces in range(0,1,1):
+    print("Programa que realiza una suma")
+    num1 = float(input("Dame un numero: "))
+    num2 = float(input("Dame otro numero: "))
+    res = (num1 + num2)
+    print("El resultado de la suma es: ",res)
